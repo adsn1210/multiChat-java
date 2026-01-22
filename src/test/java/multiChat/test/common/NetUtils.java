a76f4d82a9d366b8d67c3def2f16c303183717e5
adsn1210 <adriansalazarn@gmail.com>
2026-01-22 10:59:38 +0100

    Finalizacion de NetUtils.java

--- src/test/java/multiChat/test/common/NetUtils.java
+++ src/test/java/multiChat/test/common/NetUtils.java
@@ -1,4 +1,28 @@
 package multiChat.test.common;
 
-public class NetUtils {
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public final class NetUtils {
+
+    //Normalize.
+    public static String normalizeHost(String host){
+        if(host == null)return "";//Para que trim no pete devolvemos un "" vacio si el host llega a quedarse como null.
+        return host.trim();//El metodo trim() se utiliza para eliminar los espacion en blanco.
+    }
+    //La IP del Servidor: "Servidor con IP Local ....
+    public static String localIp(){
+        try{
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException  e) {
+            return "UnknownHostException";
+        }
+    }
+    //Para ver la IP del cliente conectado.
+    public static String safeIp(InetAddress adres){
+
+        if (adres == null)return "Unknow";
+        return adres.getHostAddress();
+    } //Donde usarlo : InetAddress adres = socket.getInetAddress(); -> IP del cliente
 }
