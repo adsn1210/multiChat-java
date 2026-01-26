5a5836648476513c76899ace25c92e2f058afde0
adsn1210 <adriansalazarn@gmail.com>
2026-01-26 19:25:58 +0100

    ChatServerMain completado

--- src/test/java/multiChat/test/server/ChatServerMain.java
+++ src/test/java/multiChat/test/server/ChatServerMain.java
@@ -1,4 +1,21 @@
 package multiChat.test.server;
 
+import multiChat.test.common.NetUtils;
+import multiChat.test.common.Protocolo;
+
+import java.sql.SQLOutput;
+
 public class ChatServerMain {
+    public static void main(String[] args) {
+        System.out.println("Servidor arrancado....Esperando conexiones (puerto ("+(Protocolo.PORT +")"));
+        System.out.println("IP Local "+(NetUtils.localIp()));
+
+        ServerLogger logger = new ServerLogger("ChatServer_log.txt");
+
+        //Creamos servidor
+        ChatServer server = new ChatServer(Protocolo.PORT,logger);
+
+        //Bucle de Escucha
+        server.start();
+    }
 }
